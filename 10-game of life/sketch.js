850e551e7372b48e52ee963a35495cb55bf67cc4
NickYuzdepski <91340642+NickYuzdepski@users.noreply.github.com>
2021-10-14 12:12:17 -0600

    started game of life

--- 10-game of life/sketch.js
+++ 10-game of life/sketch.js
@@ -1,11 +1,28 @@
-// First Demo
+// Game of Life  
+
+let grid;
+let gridSize = 10;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  grid = createEmpty2DArray(gridSize, gridSize);
 }
 
 function draw() {
   background(220);
 
-  rect(mouseX, mouseY, 100, 50);
+}
+
+
+function create2DArray(rows, cols) {
+  for (let x; x<heightX; x++) {
+    push [0];
+  }
+  for (let y; y<heightY; y++) {
+    push [0]
+  }
+  
+
+
 }
