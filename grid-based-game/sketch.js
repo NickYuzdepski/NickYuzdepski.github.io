c205dcf3f54b8f9eaecbd4e4940074a53f648d1a
NickYuzdepski <91340642+NickYuzdepski@users.noreply.github.com>
2021-10-29 12:08:34 -0600

    improved code fluency

--- grid-based-game/sketch.js
+++ grid-based-game/sketch.js
@@ -7,18 +7,37 @@ let gridSize = 10;
 let grid;
 let gridCharacterY;
 let gridCharacterX;
-let positionY = random(400);
-let positionX = random(400);
-let gridObstacle;
+let positionY = 400;
+let positionX = 400;
+let cellWidth, cellHeight;
+let speed = 5;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   grid = createEmpty2DArray(gridSize, gridSize);
+  cellWidth = width/gridSize;
+  cellHeight = height/gridSize;
 }
 
 function draw() {
   background("white");
+  
+}
 
+function mousePressed() {
+  let cellWidth = width/gridSize;
+  let cellHeight = height/gridSize;
+  let cellX = Math.floor(mouseX/cellWidth);
+  let cellY = Math.floor(mouseY/cellHeight);
+
+  if (grid[cellY][cellX] === 1) {
+    grid [cellY][cellX] = 0;
+  }
+  else if (grid[cellY][cellX] === 0) {
+    grid[cellY][cellX] = 1;
+
+  }
 }
 
 // create empty 2D array
@@ -35,41 +54,45 @@ function createEmpty2DArray(rows, cols) {
 
 // display grid
 function displayGrid() {
-  for (let y = 0; y < gridSize; y++) {
-    for (let x = 0; x < gridSize; x++) {
-      fill("white");
+  for (let y=0; y<gridSize; y++) {
+    for (let x=0; x<gridSize; x++) {
+      if (grid[y][x] === 0) {
+        fill ("white");
+      }
+      if (grid[y][x] === 1) {
+        fill ("black");
+      }
+      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
     }
   }
 }
 
-function mousePressed() {
-  let cellWidth = width/gridSize;
-  let cellHeight = height/gridSize;
-  let cellX = Math.floor(mouseX/cellWidth);
-  let cellY = Math.floor(mouseY/cellHeight);
 
-  if (grid[cellY][cellX] === 1) {
-    grid [cellY][cellX] = 0;
-  }
-  else if (grid[cellY][cellX] === 0) {
-    grid[cellY][cellX] = 1;
-  }
-}
 
 // put character in grid
-grid[gridCharacterY][gridCharacterX] = random(positionY, positionX);
+// grid[gridCharacterY][gridCharacterX] = random(positionY, positionX);
+// noStroke();
+// fill("red");
+// let choice = random(100);
+// if (choice < 25) { //up
+//   positionY -= random (speed);
+// }
+// else if (choice < 50) { //down
+//   positionY += random(speed);
+// }
+// else if (choice < 75) { //left
+//   positionX -= random(speed);
+// }
+// else if (choice < 100) { //right
+//   positionX += random(speed);
+// }
 
-fill("red");
+// // collision detection
 
-// grid obstacles
-function gridObstacles () {
-  
-}
 
-// collision detection
-if (gridCharacterY && gridCharacterX > gridObstacle) {
-  gridCharacterX += positionX && positionY;
-}
+
+
+
 
 
   
