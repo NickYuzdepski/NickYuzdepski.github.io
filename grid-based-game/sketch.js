2379b242342dff238e1fc10f6f31d20e18df3ac3
NickYuzdepski <91340642+NickYuzdepski@users.noreply.github.com>
2021-11-01 12:03:42 -0600

    added delete function

--- grid-based-game/sketch.js
+++ grid-based-game/sketch.js
@@ -1,46 +1,54 @@
-// Grid Based Game
-// Nicklas Yuzdepski
-// October 20, 2021
+//grid-based-game
+//October 21, 2021
+//Nicklas Yuzdepski
 
-// global variables
-let gridSize = 10;
+let gridDimensions = 10;
 let grid;
-let gridCharacterY;
-let gridCharacterX;
-let positionY = 400;
-let positionX = 400;
-let cellWidth, cellHeight;
-let speed = 5;
-
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  grid = createEmpty2DArray(gridSize, gridSize);
-  cellWidth = width/gridSize;
-  cellHeight = height/gridSize;
+  createCanvas(400, 400);
+  grid = createEmpty2DArray(gridDimensions, gridDimensions);
 }
 
 function draw() {
-  background("white");
-  
+  background(220);
+  displayGrid();
 }
 
+//use mouse to draw on grid
 function mousePressed() {
-  let cellWidth = width/gridSize;
-  let cellHeight = height/gridSize;
+  let cellWidth = width/gridDimensions;
+  let cellHeight = height/gridDimensions;
+
   let cellX = Math.floor(mouseX/cellWidth);
   let cellY = Math.floor(mouseY/cellHeight);
 
   if (grid[cellY][cellX] === 1) {
-    grid [cellY][cellX] = 0;
+    grid[cellY][cellX] = 0;
   }
   else if (grid[cellY][cellX] === 0) {
     grid[cellY][cellX] = 1;
-
   }
 }
 
-// create empty 2D array
+function displayGrid() {
+  let cellWidth = width/gridDimensions;
+  let cellHeight = height/gridDimensions;
+
+  for (let y=0; y<gridDimensions; y++) {
+    for (let x=0; x<gridDimensions; x++) {
+      if (grid[y][x] === 0) {
+        fill("white");
+      }
+      else if (grid[y][x] === 1) {
+        fill("black");
+      }
+      imageMode(CENTER);
+      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+    }
+  }
+}
+
 function createEmpty2DArray(rows, cols) {
   let grid = [];
   for (let y=0; y<rows; y++) {
@@ -51,44 +59,6 @@ function createEmpty2DArray(rows, cols) {
   }
   return grid;
 }
-
-// display grid
-function displayGrid() {
-  for (let y=0; y<gridSize; y++) {
-    for (let x=0; x<gridSize; x++) {
-      if (grid[y][x] === 0) {
-        fill ("white");
-      }
-      if (grid[y][x] === 1) {
-        fill ("black");
-      }
-      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
-    }
-  }
-}
-
-
-
-// put character in grid
-// grid[gridCharacterY][gridCharacterX] = random(positionY, positionX);
-// noStroke();
-// fill("red");
-// let choice = random(100);
-// if (choice < 25) { //up
-//   positionY -= random (speed);
-// }
-// else if (choice < 50) { //down
-//   positionY += random(speed);
-// }
-// else if (choice < 75) { //left
-//   positionX -= random(speed);
-// }
-// else if (choice < 100) { //right
-//   positionX += random(speed);
-// }
-
-// // collision detection
-
 
 
 
