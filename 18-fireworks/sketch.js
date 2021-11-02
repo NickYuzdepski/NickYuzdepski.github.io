d17c195e7e841f98b27050c51d6a7364e13b3a89
NickYuzdepski <91340642+NickYuzdepski@users.noreply.github.com>
2021-11-02 11:54:12 -0600

    created fireworks

---
+++ 18-fireworks/sketch.js
@@ -0,0 +1,69 @@
+// Fireworks OOP Demo
+
+let fireworks = [];
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+}
+
+function draw() {
+  background(220);
+
+  for (let i = fireworks.length-1; i >= 0; i--) {
+    if (fireworks[i].isDead()) {
+      fireworks.splice(i, 1);
+    }
+    else {
+      fireworks[i].move();
+      fireworks[i].display();
+    }
+  }
+}
+
+function mousePressed() {
+  angleMode(DEGREES);
+  let numberOfFireworks = 100;
+  let theta = 0;
+  for (let i = 0; i < 100; i++) {
+    let dx = cos(theta + random(-1, 1) * random(0, 2));
+    let dy = sin(theta) + random(-1, 1) * random(0, 2);
+  
+    
+    let myParticle = new Particle(mouseX, mouseY);
+    fireworks.push(myParticle);
+
+    theta += (360 / numberOfFireworks);
+  }
+}
+
+
+class Particle {
+  constructor(x, y, dx, dy) {
+    this.x = x;
+    this.y = y;
+    this.size = random(3, 7);
+    this.alpha = 255;
+    this.theColor = color(255, 0, 0, this.alpha);
+    this.dx = dx; //random(-5, 5);
+    this.dy = dy; //random(-5, 5);
+  }
+
+  display() {
+    noStroke();
+    fill(this.theColor);
+    circle(this.x, this.y, this.size);
+  }
+
+  move() {
+    this.alpha--;
+    this.theColor = color(255, 0, 0, this.alpha);
+    this.x += this.dx;
+    this.y += this.dy;
+  }
+
+  isDead() {
+    return this.alpha <0;
+  }
+}
