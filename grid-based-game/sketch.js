237586f5e610fb913726c6c4c46763452b2d18e7
NickYuzdepski <91340642+NickYuzdepski@users.noreply.github.com>
2021-11-04 12:12:23 -0600

    created character movement and started grid projectiles

--- grid-based-game/sketch.js
+++ grid-based-game/sketch.js
@@ -9,7 +9,6 @@ let y;
 let playerX = 0;
 let playerY = 0;
 
-
 function setup() {
   createCanvas(400, 400);
   grid = createEmpty2DArray(gridSize, gridSize);
@@ -19,25 +18,9 @@ function setup() {
 function draw() {
   background(220);
   displayGrid();
-
 }
 
-//use mouse to draw on grid
-function mousePressed() {
-  let cellWidth = width/gridSize;
-  let cellHeight = height/gridSize;
-
-  let cellX = Math.floor(mouseX/cellWidth);
-  let cellY = Math.floor(mouseY/cellHeight);
-
-  if (grid[cellY][cellX] === 1) {
-    grid[cellY][cellX] = 0;
-  }
-  else if (grid[cellY][cellX] === 0) {
-    grid[cellY][cellX] = 1;
-  }
-}
-
+//draw grid
 function displayGrid() {
   let cellWidth = width/gridSize;
   let cellHeight = height/gridSize;
@@ -56,6 +39,7 @@ function displayGrid() {
   }
 }
 
+//create array
 function createEmpty2DArray(rows, cols) {
   let grid = [];
   for (let y = 0; y < rows; y++) {
@@ -67,25 +51,48 @@ function createEmpty2DArray(rows, cols) {
   return grid;
 }
 
-function moveCircle(circleX, circleY)
-//edge boundary
-  if (circleX >= 0 && circleY && circleX >= gridsize && circleY >= gridsize) {
-    if (grid [circleX][circleY] === 0) {
+function moveCircle(diffX, diffY) {
+  let diffX = playerX + 1;
+  let diffY = playerY + 1;
 
+//edge boundary
+  if (diffX >= 0 && diffY && diffX >= gridSize && diffY >= gridSize) {
+    if (grid [diffX][diffY] === 0) {
+//player allowed to move when grid value is 0
+      grid[diffX][diffY] === 9;
     }
+//player not allowed to move when grid value is 1
+    if (grid [diffX][diffY] === 1) {
+      grid[diffX][diffY] === 1;
+      }
+//character movement
+  keyPressed() {
+    if (keyCode === "w") {
+      grid[playerX][playerX] += 1;
+    }
+    if (keyCode === "s") {
+      grid[playerX][playerX] -= 1;
+    }
+    if (keyCode === "d") {
+      grid[playerX][playerX] += 1;
+    }
+    if (keyCode === "a") {
+      grid[playerX][playerX] -= 1;
+    }
+  }
+}
+}
+
+//create grid projectiles
+function gridProjectiles(gridsize, gridsize)
+  for (let y = 0; y < gridSize.length; y++) {
+    for (let x = 0; x < gridSize.length; x++) {
   }
 
 
 
 
-
-
   
-
-
-
-
-
 
 
 
